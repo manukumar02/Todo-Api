dc86713d0d552eece66487162fea96c06084a8b2
Manu Sharma <manukumar02@gmail.com>
2016-06-14 10:07:59 +0530

    Add PUT todo route

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/todos/:id', function(req, res) {
 	} else {
 		res.status(404).send();
 	}
-	//res.send('Asking TODO with id of ' + req.params.id )
+	//res.send('Asking Todo with id of ' + req.params.id )
 });
 
 app.post('/todos', function(req, res) {
@@ -56,6 +56,31 @@ app.delete('/todos/:id', function(req, res) {
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
+})
+
+app.put('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if(!matchedTodo) {
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} 
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 })
 
 app.listen(PORT, function() {
